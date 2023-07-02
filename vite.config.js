53265941f4096e4fe7424af96fbd889672f96c4e
Ido Tsehori <ido.tsehori@gmail.com>
2023-07-02 15:10:30 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://idoTsehori.github.io/Ido-Tsehori-28-6-2023-/',
+  base: 'https://idoTsehori.github.io/Ido-Tsehori-28-6-2023-/',
   plugins: [vue()],
   build: {
     chunkSizeWarningLimit: 1000,
