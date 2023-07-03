1158527399812754d0ce76c7c3ebe34bc75ac735
Ido Tsehori <ido.tsehori@gmail.com>
2023-07-03 23:04:01 +0300

    deploy

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,16 +1,14 @@
-<template>Video</template>
+<template>
+  <div class="flex column align-center">
+    <h1>App Demo</h1>
+    <button style="width: 5em; border-radius: 50%">
+      <a href="https://youtu.be/tMwM42LtJ-w" target="_blank">Click here to see the demo</a>
+    </button>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'Video',
-  data() {
-    return {}
-  },
-  methods: {},
-  computed: {},
-  created() {},
-  components: {},
 }
 </script>
-
-<style></style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Ido-Tsehori-28-6-2023-/',
   plugins: [vue()],
